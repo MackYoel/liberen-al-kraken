83f309caf05b632e3f4bf3d4a8e2312975e2961f
MackYoel <yoel.zalas@gmail.com>
2018-02-20 03:33:31 -0500

    heroku configs

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn liberenalkraken.wsgi --log-file -
--- liberenalkraken/settings/heroku.py
+++ liberenalkraken/settings/heroku.py
@@ -1,3 +1,15 @@
+import dj_database_url
+
 from .base import *
 
 DEBUG = False
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+WHITENOISE_STATIC_PREFIX = STATIC_URL
+
+STATIC_URL = os.environ.get('DJANGO_STATIC_HOST', STATIC_URL)
